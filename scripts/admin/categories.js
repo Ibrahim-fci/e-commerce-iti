427ceefb23e7676a56a31c5ea6278564713f3ce4
Ibrahim Ismail <51814249+Ibrahim-fci@users.noreply.github.com>
2023-12-04 19:03:57 +0200

    feat: categories_crud_operations

--- scripts/admin/categories.js
+++ scripts/admin/categories.js
@@ -22,7 +22,7 @@ function renderCategories() {
           <div class="product-title"  category = "${category}">${category}</div>
           <div class="action-buttons">
             <button class="update-button" onclick="updatePopup('${category}')">Update</button>
-            <button class="delete-button" onclick="">Delete</button>
+            <button class="delete-button" onclick="deletePopup('${category}')">Delete</button>
           </div>
         </div>
       </div>
@@ -49,6 +49,25 @@ window.addEventListener("DOMContentLoaded", function () {
   renderCategories();
 });
 
+/********************************** ADD A NEW CATEGORIES */
+function addCategory() {
+  var addForm = document.getElementById("addForm");
+  var categories = getCategoriess();
+
+  // check if category already exists
+  if (getProduct(addForm.categoryName.value)) {
+    alert("Category already exists");
+    return;
+  }
+
+  categories.push(addForm.categoryName.value);
+  localStorage.setItem("categories", JSON.stringify(categories));
+  //   renderCategories();
+}
+
+var addProductBtn = document.getElementById("addProductBtn");
+addProductBtn.addEventListener("click", addCategory);
+
 /************************************ ADD CATEGORIES */
 // adding product popup
 function openPopup() {
@@ -71,4 +90,72 @@ function updatePopup(category) {
 
 function closeUpdatePopup() {
   document.getElementById("updateForm-popup").style.display = "none";
+}
+
+/**************** UPDATE CATEGORIES */
+
+function updateCategory() {
+  var oldCategoryName = document.getElementById("update-productId-input");
+  var updateForm = document.getElementById("updateForm");
+  var categories = getCategoriess();
+  var products = getAllProducts();
+
+  var index = categories.findIndex(
+    (category) => category == oldCategoryName.value
+  );
+
+  categories[index] = updateForm.categoryName.value;
+
+  // update all products with this category to the new category
+  products = products.map((product) => {
+    if (product.category == oldCategoryName.value) {
+      product.category = updateForm.categoryName.value;
+    }
+    return product;
+  });
+
+  oldCategoryName.value = updateForm.categoryName.value;
+  localStorage.setItem("categories", JSON.stringify(categories));
+  localStorage.setItem("products", JSON.stringify(products));
+}
+
+var updateCategoryBtn = document.getElementById("updateCategoryBtn");
+updateCategoryBtn.addEventListener("click", updateCategory);
+
+// get all productss
+function getAllProducts() {
+  return (products = JSON.parse(localStorage.getItem("products"))
+    ? JSON.parse(localStorage.getItem("products"))
+    : []);
+}
+
+/********************* DELETE CATEGORIES */
+function deletePopup(category) {
+  var categoryNamePopup = document.getElementById("delete-categoryName");
+  var iddenInput = document.getElementById("delete-categort-hidden-input");
+  iddenInput.value = category;
+  categoryNamePopup.innerHTML = category;
+  document.getElementById("delete-popup").style.display = "flex";
+}
+
+function closeDeletePopup() {
+  document.getElementById("delete-popup").style.display = "none";
+}
+
+function deleteCategory() {
+  var iddenInput = document.getElementById("delete-categort-hidden-input");
+  var categories = getCategoriess();
+  var products = getAllProducts();
+
+  var index = categories.findIndex((category) => category == iddenInput.value);
+
+  // delete all products with this category
+  products = products.filter((product) => product.category != iddenInput.value);
+  categories.splice(index, 1); // remove category
+
+  localStorage.setItem("categories", JSON.stringify(categories));
+  localStorage.setItem("products", JSON.stringify(products));
+
+  closeDeletePopup();
+  renderCategories();
 }
